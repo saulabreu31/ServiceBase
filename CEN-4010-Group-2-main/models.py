e7974092a87fd4ad94f1946d2b4cce1eefc8a48e
saulabreu31 <53130862+saulabreu31@users.noreply.github.com>
2025-05-30 20:27:42 -0400

    class forumpost

--- CEN-4010-Group-2-main/models.py
+++ CEN-4010-Group-2-main/models.py
@@ -38,6 +38,14 @@ class Note(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.now())
+    
+class ForumPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    image_filename = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Studyflow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
